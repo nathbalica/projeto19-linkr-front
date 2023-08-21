df329cdd89a19830a6852f0f0b5f93c0118ac088
nathbalica <nathbalica@gmail.com>
2023-08-21 15:55:08 -0300

    mobile ok search bar

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -69,44 +69,64 @@ export default function UserSearch({token}) {
 }
 
 const SearchInput = styled(DebounceInput)`
-  width: 563px;
-  height: 35px;
-  padding: 8px;
-  border: none;
-  border-radius: 20px;
-  background-color: #f5f5f5;
-  outline: none;
+
+ @media screen and (min-width: 768px) {
+
+   width: 563px;
+   height: 35px;
+   padding: 8px;
+   border: none;
+   border-radius: 20px;
+   background-color: #f5f5f5;
+   outline: none;
+ }
 `;
 
 const SearchContainer = styled.div`
+@media screen and (max-width: 767px) {
+    display: none; /* Hide the SearchBar on smaller screens */
+  }
+
+@media screen and (min-width: 768px) {
+  display: block;
   margin-top: 20px;
   position: relative;
+}
 `;
 
 const SearchIcon = styled.span`
+
+@media screen and (min-width: 768px) {
+
   position: absolute;
   top: 50%;
   right: 10px;
   transform: translateY(-50%);
   cursor: pointer;
+}
 `;
 
 const SuggestionsContainer = styled.div`
-    position: absolute;
-    top: 35.5px;
-    left: 15px;
-    right: 0;
-    background-color: #EFEFEF;
-    border: none;
-    border-bottom-left-radius: 4px;
-    border-bottom-right-radius: 4px;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-    z-index: 1000;
-    height: auto;
-    width: 525px;
+@media screen and (min-width: 768px) {
+
+  position: absolute;
+  top: 35.5px;
+  left: 15px;
+  right: 0;
+  background-color: #EFEFEF;
+  border: none;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+  height: auto;
+  width: 525px;
+}
 `;
 
 const Suggestion = styled.div`
+@media screen and (min-width: 768px) {
+
   display: flex;
   align-items: center;
   padding: 8px 16px;
@@ -115,17 +135,23 @@ const Suggestion = styled.div`
   &:hover {
     background-color: #f5f5f5;
   }
+}
 `;
 
 const SuggestionImage = styled.img`
+@media screen and (min-width: 768px) {
+  
   width: 30px;
   height: 30px;
   border-radius: 50%;
   margin-right: 10px;
-`;
+}
+`
 
 const SuggestionUsername = styled.div`
+@media screen and (min-width: 768px) {
   font-weight: bold;
+  }
 `;
 
 
